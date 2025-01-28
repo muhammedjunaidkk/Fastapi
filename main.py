ae6f714d46ead9599c53ca3be6bc844c23feb5ec
Muhammed Junaid K K <muhammedjunaid.kk@tnpconsultants.com>
2025-01-28 12:41:30 +0530

    testing

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     
-    return("hello DIVA bbb  aaa..............................")
+    return("hello DIVA bbb  aaa ccc..............................")
 
