5908c052cd4875cea0aef9e671b0a2e93cb24f4c
Muhammed Junaid K K <muhammedjunaid.kk@tnpconsultants.com>
2025-03-03 12:27:09 +0530

    uat testing

--- main.py
+++ main.py
@@ -1,27 +1,11 @@
-from fastapi import FastAPI, HTTPException
-from database import execute_insert
-import os
+from fastapi import FastAPI
 
 app = FastAPI()
 
 @app.get("/")
-def add_name(name: str = "Jihana"):
-    """Inserts a name into the appropriate table."""
-    
-    # Get the environment variable 'ENV' (no default because it will be set via Docker)
-    env = os.getenv("ENV").upper()  # No default, because ENV is guaranteed to be set
-    
-    # Get schema and table names based on the environment variable
-    schema = os.getenv(f"SCHEMA_{env}")  # Default to "uatdata" if not set
-    table_name = os.getenv(f"TABLE_{env}")  # Default to "uatinfo" if not set
-    
-    # Log the environment for debugging
-    print(f"Running in {env} environment.")  # This helps you confirm the environment.
-    
-    data = {"name": name}
-    
-    try:
-        execute_insert(table_name, data, schema)
-        return {"message": f"Name '{name}' added to {table_name} in {schema} schema."}
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+def read_root():
+    return {"message": "Hello, FastAPI!"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int):
+    return {"item_id": item_id, "message": "This is your item"}
